bd23d237fe0271f08605559c5a6c0fed6e33f6f8
Thalitalima23 <thalitalima23@gmail.com>
2025-12-03 13:23:34 -0300

    :sparkles: Adicionando page Sobre_o_Projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import SobreProjeto from "./pages/sobre_projeto/SobreProjeto"
 
 function App() {
 	return (
 		<>
+		< SobreProjeto />
 		< Navbar/>
 		< Home />
 		< Footer/>

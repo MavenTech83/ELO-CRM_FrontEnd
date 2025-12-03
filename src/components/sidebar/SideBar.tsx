4c56101cba0c3f7c6b38374f2e9265dadcc31099
Ana Lemos <carol1692@hotmail.com>
2025-12-03 14:46:38 -0300

    sidebar com pagina ativa e aumento dos icones

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,58 +1,64 @@
-import { HandshakeIcon, HeartIcon,HouseIcon, ListIcon, SignOutIcon, UserIcon, UsersFourIcon } from "@phosphor-icons/react";
+import { HandshakeIcon, HeartIcon, HouseIcon, ListIcon, SignOutIcon, UserIcon, UsersFourIcon } from "@phosphor-icons/react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <>
-      <div className={`h-screen  rounded-3xl bg-(--color-picton-blue-950) text-white flex flex-col p-4 transition-all duration-300 
+    <div className={`h-screen rounded-3xl bg-(--color-picton-blue-950) text-white flex flex-col p-4 transition-all duration-300 
       ${isOpen ? "w-64" : "w-20"}`}>
 
       {/* Botão de expandir/recolher */}
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="mb-6 flex items-center gap-2 hover:bg-(--color-picton-blue-800) p-2 rounded-lg"
+        className="mb-6 flex items-center gap-2 hover:bg-(--color-picton-blue-800) active:bg-(--color-picton-blue-800) p-2 rounded-lg"
       >
-       <ListIcon size={24} />
+        <ListIcon size={34} />
         {isOpen && <span className="text-lg font-semibold">Menu</span>}
       </button>
 
       {/* Navegação */}
       <nav className="flex flex-col gap-3">
 
-        <Link to='/home'>
-          <SidebarItem icon={<HouseIcon size={22} />} label="Home" isOpen={isOpen} />
-        </Link>
-        
-        <Link to='/perfil'>
-          <SidebarItem icon={<UserIcon size={22} />} label="Perfil" isOpen={isOpen} />
-        </Link>
-        
-        <Link to='/clientes'>
-          <SidebarItem icon={<UsersFourIcon size={22} />} label="Clientes" isOpen={isOpen} />
-        </Link>
-        
-        <Link to='/oportunidades'>
-           <SidebarItem icon={<HandshakeIcon size={22} />} label="Oportunidade" isOpen={isOpen} />
-        </Link>
+        <NavLink to="/home">
+          {({ isActive }) => (
+            <SidebarItem icon={<HouseIcon size={32} />} label="Home" isOpen={isOpen} active={isActive} />
+          )}
+        </NavLink>
 
-        <Link to='/sobrenos'>
-           <SidebarItem icon={<HeartIcon size={22} />} label="Sobre Nós" isOpen={isOpen} />
-        </Link>
-       
-        <Link to='/'>
-           <SidebarItem icon={<SignOutIcon size={32} />} label="Sair" isOpen={isOpen} />
-        </Link>
-       
+        <NavLink to="/perfil">
+          {({ isActive }) => (
+            <SidebarItem icon={<UserIcon size={32} />} label="Perfil" isOpen={isOpen} active={isActive} />
+          )}
+        </NavLink>
+
+        <NavLink to="/clientes">
+          {({ isActive }) => (
+            <SidebarItem icon={<UsersFourIcon size={32} />} label="Clientes" isOpen={isOpen} active={isActive} />
+          )}
+        </NavLink>
+
+        <NavLink to="/oportunidades">
+          {({ isActive }) => (
+            <SidebarItem icon={<HandshakeIcon size={32} />} label="Oportunidade" isOpen={isOpen} active={isActive} />
+          )}
+        </NavLink>
+
+        <NavLink to="/sobrenos">
+          {({ isActive }) => (
+            <SidebarItem icon={<HeartIcon size={32} />} label="Sobre Nós" isOpen={isOpen} active={isActive} />
+          )}
+        </NavLink>
+
+        <NavLink to="/">
+          {({ isActive }) => (
+            <SidebarItem icon={<SignOutIcon size={32} />} label="Sair" isOpen={isOpen} active={isActive} />
+          )}
+        </NavLink>
 
       </nav>
-
     </div>
-    
-    </>
-    
   );
 }
 
@@ -60,14 +66,22 @@ type PropsItem = {
   icon: React.ReactNode;
   label: string;
   isOpen: boolean;
+  active: boolean;
 };
 
-function SidebarItem({ icon, label, isOpen }: PropsItem) {
+function SidebarItem({ icon, label, isOpen, active }: PropsItem) {
   return (
     <div
-      className="flex items-center gap-3 p-2 rounded-lg cursor-pointer hover:bg-(--color-picton-blue-800) transition-colors"
+      className={`
+        flex items-center gap-3 p-2 rounded-lg cursor-pointer transition-colors
+        ${active ? "bg-(--color-picton-blue-800) text-(--color-picton-blue-500)" : "hover:bg-(--color-picton-blue-800)"}
+      `}
     >
-      {icon}
+      {/* Ícone com cor diferente se ativo */}
+      <div className={active ? "text-(--color-picton-blue-500)" : ""}>
+        {icon}
+      </div>
+
       {isOpen && <span className="text-base">{label}</span>}
     </div>
   );

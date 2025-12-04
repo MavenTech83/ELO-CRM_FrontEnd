ffbe0dd15d5166dc992b1340498e353d247a440f
jessicatinguely <jessicatinguely@icloud.com>
2025-12-04 08:48:53 -0300

    :sparkles: Ajustado App.tsx para ficar conforme DEVELOP

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 
-import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./pages/login/Login"
+import Oportunidades from "./pages/oportunidades/Oportunidades"
+import Perfil from "./pages/perfil/Perfil"
+import Clientes from "./pages/clientes/Clientes"
+import SobreNos from "./pages/sobrenos/SobreNos"
 import Home from "./pages/home/Home"
+import { AuthProvider } from "./contexts/AuthContext"
+import { ToastContainer } from "react-toastify"
+import Editar from "./pages/editar/Editar"
+import Deletar from "./pages/deletar/Deletar"
 
 function App() {
 	return (
 		<>
-		< Navbar/>
-		< Home />
-		< Footer/>
-		
-
+				<AuthProvider>
+				<ToastContainer />
+				<BrowserRouter>
+					<Routes>
+							<Route path="/" element={<Login />} />
+							<Route path="/home" element={< Home />} />
+							<Route path="/oportunidades" element={< Oportunidades />} />
+							<Route path="/editaroportunidade/:id" element={<Editar />} />
+							<Route path="/deletaroportunidade/:id" element={<Deletar />} />
+							<Route path="/perfil" element={<Perfil />} />
+							<Route path="/clientes" element={<Clientes />} />
+							<Route path="/sobrenos" element={<SobreNos />} />
+					</Routes>
+				</BrowserRouter>
+				</AuthProvider>
+  	
 		</>
 	)
 }

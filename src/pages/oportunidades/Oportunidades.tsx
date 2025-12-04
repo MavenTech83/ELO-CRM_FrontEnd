d38a32deee70eb138cdd9030291ce044e2e9de1f
jessicatinguely <jessicatinguely@icloud.com>
2025-12-04 17:17:13 -0300

    :sparkles: Scroll Oportunidade

--- src/pages/oportunidades/Oportunidades.tsx
+++ src/pages/oportunidades/Oportunidades.tsx
@@ -7,20 +7,22 @@ import type Oportunidade from "../../models/Oportunidade";
 
 function Oportunidades() {
   const [selectedCard, setSelectedCard] = useState<Oportunidade | null>(null);
-  
+
   return (
     <Layout>
-        <div className="h-full flex items-center justify-center gap-2">
-                {/* <!-- Coluna 1 --> */}
-                <div className="w-1/2 p-4 pb-2 rounded-4xl shadow h-full overflow-y-auto scroll-smooth">
-                     <ListaOportunidade onSelect={setSelectedCard} />
-                </div>
+      <div className="h-full flex items-stretch gap-2 min-h-0">
 
-                {/* <!-- Coluna 2 --> */}
-               <div className="bg-white/20 w-1/2 p-4 rounded-4xl shadow h-full overflow-y-auto">
-                  <DetalhesOportunidade oportunidade={selectedCard} />
-              </div>
+        {/* Coluna 1 */}
+        <div className="w-1/2 p-4 pb-2 rounded-4xl shadow min-h-0 overflow-y-auto">
+          <ListaOportunidade onSelect={setSelectedCard} />
         </div>
+
+        {/* Coluna 2 */}
+        <div className="w-1/2 p-4 pb-2 rounded-4xl shadow min-h-0 overflow-y-auto">
+          <DetalhesOportunidade oportunidade={selectedCard} />
+        </div>
+
+      </div>
 
     </Layout>
   )
